dbf70163dbe7d23c214d04140921a9e028f576e2
Ehsan <ehsanpc999@gmail.com>
2025-02-18 10:34:05 +0300

    fix bug

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:weather_pod/features/weather/presentation/screens/home_page.dart';
+import 'package:weather_pod/features/weather/presentation/screens/home_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
