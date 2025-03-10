1fbd0ab110e2ba243cb88d4b63227c5364c49c7b
Ehsan <ehsanpc999@gmail.com>
2025-03-10 21:58:30 +0300

    fix internetConnectionProvider

--- lib/features/weather/data/riverpod/internet_connection_provider.dart
+++ lib/features/weather/data/riverpod/internet_connection_provider.dart
@@ -2,18 +2,14 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
-// final internetConnectionProvider = StreamProvider<bool>((ref) {
-//   return InternetConnection().onStatusChange.map(
-//         (status) => status == InternetStatus.connected,
-//       );
-// });
-
-// final internetConnectionProvider = FutureProvider<bool>((ref) async {
-//   var connectivityResult = await Connectivity().checkConnectivity();
-//   return connectivityResult != ConnectivityResult.none;
-// });
-
-final internetConnectionProvider = FutureProvider<bool>((ref) async {
-  var connectivityResult = await Connectivity().checkConnectivity();
-  return !connectivityResult.contains(ConnectivityResult.none);
+//the StreamProvider to continuously monitor the connection status
+final internetConnectionProvider = StreamProvider<bool>((ref) {
+  return Connectivity().onConnectivityChanged.map(
+    (connectivityResults) {
+      // Check if any of the connectivity results indicate an active connection
+      return connectivityResults.any(
+        (result) => result != ConnectivityResult.none,
+      );
+    },
+  );
 });
